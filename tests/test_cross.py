348f9024713ab3ef1f65b7b96a67ee5dbd55930e
younik <omar.younis98@gmail.com>
2022-09-15 10:23:49 +0200

    update test

--- tests/test_cross.py
+++ tests/test_cross.py
@@ -1,14 +1,21 @@
 from breeding_gym.breeding_gym import BreedingGym
 import numpy as np
+from breeding_gym.simulator.simulator import BreedingSimulator
+from breeding_gym.utils.paths import DATA_PATH
+import pytest
 
 
-def test_cross_r():
+@pytest.mark.parametrize("idx", [0, 1])
+def test_cross_r(idx):
     def const_co_mask(self, n_progenies, chr_idx):
-        marker_per_chr = self.recombination_vectors[chr_idx].shape[0]
-        return np.ones((n_progenies, marker_per_chr, 2), dtype="int")
-    BreedingGym._get_crossover_mask = const_co_mask
+        marker_per_chr = self.r_vectors[chr_idx].shape[0]
+        return idx * np.ones((n_progenies, marker_per_chr, 2), dtype="int")
+    BreedingSimulator._get_crossover_mask = const_co_mask
 
-    env = BreedingGym()
+    env = BreedingGym(
+        initial_population=DATA_PATH.joinpath("smaller_geno.txt"),
+        genetic_map=DATA_PATH.joinpath("smaller_genetic_map.txt"),
+    )
     init_pop = env.reset()
     p0, p1 = init_pop[0], init_pop[1]
     assert p0.shape == p1.shape
@@ -18,13 +25,15 @@ def test_cross_r():
     assert new_pop.shape == (1, p0.shape[0], 2)
 
     ind = new_pop[0]
-    assert np.all(ind[:, 0] == p0[:, 1])
-    assert np.all(ind[:, 1] == p1[:, 1])
+    assert np.all(ind[:, 0] == p0[:, idx])
+    assert np.all(ind[:, 1] == p1[:, idx])
 
 
 def test_caching():
-    env = BreedingGym()
-
+    env = BreedingGym(
+        initial_population=DATA_PATH.joinpath("smaller_geno.txt"),
+        genetic_map=DATA_PATH.joinpath("smaller_genetic_map.txt"),
+    )
     env.reset(return_info=False)
 
     GEBV = env.GEBV
