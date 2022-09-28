3582bdf6f11d10caa7c02bf472a9b45c6acb0334
Omar Younis <omar.younis98@gmail.com>
2022-09-28 16:30:45 +0200

    add profile script

---
+++ scripts/profile_cross.py
@@ -0,0 +1,19 @@
+import cProfile
+from breeding_gym.simulator.simulator import BreedingSimulator
+import numpy as np
+from breeding_gym.utils.paths import DATA_PATH
+
+
+n_progenies = 400
+n_markers = 150_000
+
+parents = np.random.choice(a=[False, True], size=(n_progenies, 2, n_markers, 2), p=[0.5, 0.5])
+simulator = BreedingSimulator(
+    genetic_map=DATA_PATH.joinpath("medium_genetic_map.txt")
+)
+
+with cProfile.Profile() as pr:
+    simulator.cross(parents)
+
+pr.dump_stats('profile.prof')
+
